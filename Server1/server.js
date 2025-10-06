11d6fb4f285684c22922bc1e73762a01d02ffdad
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-10-06 14:40:29 +0530

    done

--- Server1/server.js
+++ Server1/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 // ----------- Middleware ------------
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173", "https://nasa-iss-space-surfers.vercel.app"],
 }));
 
 // ----------- Helpers ------------
